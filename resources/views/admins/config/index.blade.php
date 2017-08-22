c1afc6184d94dc9c89166f58fbbfe9bfb4e5fb18
haibo <604657761@qq.com>
2017-08-22 15:45:24 +0800

    17

--- resources/views/admins/config/index.blade.php
+++ resources/views/admins/config/index.blade.php
@@ -7,43 +7,43 @@
 
 @section('content')
 
-    <div class="mws-panel grid_8">
+    <div class="mws-panel grid_3">
         <div class="mws-panel-header">
-            <span><i class="icon-users"></i>网站配置</span>
+            <span><i class="icon-book"></i>系统配置信息详情</span>
         </div>
         <div class="mws-panel-body no-padding">
-            <div id="DataTables_Table_1_wrapper" class="dataTables_wrapper" role="grid">
-
-                <table aria-describedby="DataTables_Table_1_info" id="DataTables_Table_1"
-                       class="mws-datatable-fn mws-table dataTable">
-                    <thead>
-                    <tr role="row">
-                        <th>操作系统</th>
-                        <th>环境</th>
-                        <th>服务器</th>
-                        <th>时间</th>
-                        <th>ip</th>
-                    </tr>
-                    </thead>
-                    <tbody aria-relevant="all" aria-live="polite" role="alert">
-
-                        <tr class=" odd ">
-                            <td align="center" class="  sorting_1">{{$data->oprate_system}}</td>
-                            <td align="center" class=" ">{{$data->environment}}</td>
-                            <td align="center" class=" ">{{$data->php_server}}</td>
-                            <td align="center" class=" ">{{date('Y-m-d H:i:s',time())}}</td>
-                            <td align="center" class=" ">{{$data->ip}}</td>
-
-                        </tr>
-                    </tbody>
-                </table>
-
-
-
-
-
-
-            </div>
+            <ul class="mws-summary clearfix">
+                <li>
+                    <span class="key"><i class="icon-support"></i> 操作系统</span>
+                    <span class="val">
+                                    <span class="text-nowrap">{{$data->oprate_system}}</span>
+                                </span>
+                </li>
+                <li>
+                    <span class="key"><i class="icon-certificate"></i> 环境</span>
+                    <span class="val">
+                                    <span class="text-nowrap">{{$data->environment}} <i class="up icon-arrow-up"></i></span>
+                                </span>
+                </li>
+                <li>
+                    <span class="key"><i class="icon-shopping-cart"></i> 服务器</span>
+                    <span class="val">
+                                    <span class="text-nowrap">{{$data->php_server}} <i class="down icon-arrow-down"></i></span>
+                                </span>
+                </li>
+                <li>
+                    <span class="key"><i class="icon-install"></i> 时间</span>
+                    <span class="val">
+                                    <span class="text-nowrap">{{date('Y-m-d H:i:s',time())}}</span>
+                                </span>
+                </li>
+                <li>
+                    <span class="key"><i class="icon-key"></i> ip</span>
+                    <span class="val">
+                                    <span class="text-nowrap">{{$data->ip}}</span>
+                                </span>
+                </li>
+            </ul>
         </div>
     </div>
 @endsection
